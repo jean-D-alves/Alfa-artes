73f550c9e9e0969941052e3d460d4e436225bfcf
jean-D-alves <jenavinicius64@gmail.com>
2025-06-21 10:34:44 -0300

    Alfa&artes0.2

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ function enviarmensagem(texto) {
     const link = 'https://wa.me/${numero}?text=${mensagem}'
     window.open(link, "_blank")
 }
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
